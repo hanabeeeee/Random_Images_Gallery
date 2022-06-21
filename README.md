cbaf0f5d9aaf568cc04d996f67609a6637016a64
hanabeeeee <105402245+hanabeeeee@users.noreply.github.com>
2022-06-21 12:35:16 +0900

    Add files via upload

--- README.md
+++ README.md
@@ -1,9 +1,5 @@
 # Random Image Gallery
 
-<img width="1440" alt="스크린샷 2022-06-21 12 30 09" src="https://user-images.githubusercontent.com/105402245/174710323-e8954d50-9868-42f9-9ece-df8ab29da376.png">
-
-https://hanabeeeee.github.io/Random_Images_Gallery/
-
 ## 소개
 - 랜덤하게 이미지가 변경되는 갤러리
 - 시간도 표시됨
--- js/app.js
+++ js/app.js
@@ -9,6 +9,22 @@ const img_url = [
 ];
 
 $(function(){
+
+    //년,월,일
+    let d = new Date();
+    let year = d.getFullYear();
+    let month = d.getMonth() + 1;
+    let date = d.getDate();
+
+    console.log(year, month, date);
+
+    //html에 날짜 표시
+    $('.year').html(year);
+    $('.month').html(month);
+    $('.date').html(date);
+    
+
+
     function changeBg() {
       let imgNum = Math.floor(Math.random() * 6)
       console.log(imgNum);
